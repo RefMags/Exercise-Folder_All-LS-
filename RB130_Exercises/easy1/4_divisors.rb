b9b7ef921c86f12ffc0bb7fb1532efac5075384b
Ref Mags <fyfymags@gmail.com>
2026-01-20 20:03:53 +0900

    a repo for all the exercise from LS

---
+++ RB130_Exercises/easy1/4_divisors.rb
@@ -0,0 +1,16 @@
+def divisors(number)
+   result = []
+
+   (1..number).each do |num|
+     result << num if number % num == 0
+   end
+
+   result
+end
+
+
+p divisors(1) == [1]
+p divisors(7) == [1, 7]
+p divisors(12) == [1, 2, 3, 4, 6, 12]
+p divisors(98) == [1, 2, 7, 14, 49, 98]
+p divisors(99400891) == [1, 9967, 9973, 99400891]
